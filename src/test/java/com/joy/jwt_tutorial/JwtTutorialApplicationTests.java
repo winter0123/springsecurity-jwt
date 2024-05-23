a2ef1943f404071f1312422b9bebfc872e1f11a8
winter0123 <cloudmist.j@gmail.com>
2024-05-23 20:42:00 +0900

    init

---
+++ src/test/java/com/joy/jwt_tutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joy.jwt_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
